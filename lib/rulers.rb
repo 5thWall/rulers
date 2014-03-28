d9af6ca2b61831f68a84c3b4de2591de0f1725a6
Andy <5thWall@gmail.com>
2014-03-27 20:25:59 -0700

    Add routes stuff

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,10 +1,33 @@
 require "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      [200, {'Contet-Type' => 'text/html'},
-       ["Hello from Ruby on Rulers!"]]
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      elsif env['PATH_INFO'] == '/'
+        env['PATH_INFO'] = '/home/index'
+      end
+
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+
+      begin
+        text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'},
+         [text]]
+      rescue Exception => e
+        [500, {'Content-Type' => 'text/html'}, [e.message]]
+      end
+    end
+  end
+
+  class Controller
+    attr_reader :env
+
+    def initialize(env)
+      @env = env
     end
   end
 end
