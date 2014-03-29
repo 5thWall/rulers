1ff995c8c34e3ad704a5ae058691e7eb2860cbef
Andy <5thWall@gmail.com>
2014-03-29 13:16:31 -0700

    Inject instance variables into view

--- lib/rulers/controller.rb
+++ lib/rulers/controller.rb
@@ -14,7 +14,7 @@ module Rulers
       template = File.read filename
       eruby = Erubis::Eruby.new(template)
 
-      eruby.result locals.merge(env: env)
+      eruby.result locals.merge(instance_vars_hash).merge(env: env)
     end
 
     def controller_name
@@ -22,6 +22,14 @@ module Rulers
       klass = klass.to_s.gsub /Controller$/, ''
 
       Rulers.to_snake_case(klass)
+    end
+
+    private
+
+    def instance_vars_hash
+      self.instance_variables.reduce({}) do |hash, var|
+        hash.merge(var => self.instance_variable_get(var))
+      end
     end
   end
 end
