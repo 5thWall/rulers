b841bb39a4f34e4c45988354c0d04ce6028b530a
Andy <5thWall@gmail.com>
2014-03-29 12:33:13 -0700

    Update test

--- test/spec/application_test.rb
+++ test/spec/application_test.rb
@@ -2,7 +2,16 @@ require_relative '../test_helper'
 
 include Rack::Test::Methods
 
+class TestController < Rulers::Controller
+  def index
+    "Hello!"
+  end
+end
+
 class TestApp < Rulers::Application
+  def get_controller_and_action(env)
+    [TestController, 'index']
+  end
 end
 
 def app
@@ -12,7 +21,7 @@ end
 describe TestApp do
   describe "test_request" do
     it "Returns hello" do
-      get "/"
+      get "/example/route"
 
       last_response.must_be :ok?
       last_response.body.must_be :include?, "Hello"
